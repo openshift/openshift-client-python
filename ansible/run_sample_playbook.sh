b70fb8aa561eafd7ab41f795314d8295b71f8704
Justin Pierce <jupierce@redhat.com>
2019-07-11 14:13:53 -0400

    sa token extract and ansible update

--- ansible/run_sample_playbook.sh
+++ ansible/run_sample_playbook.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "The following sample assumes that 'oc' is on the localhost and can communicate with an OpenShift cluster"
+if [[ -z "$1" ]]; then
+    echo "Hostname is required for this operation"
+    echo "Example: $0 int-2"
+    exit 1
+fi
+
+echo "The following sample assumes that 'oc' is on the localhost and can communicate with an OpenShift cluster specified"
 echo "Running..."
 # export PYTHONPATH="$(pwd)/../packages"
-ansible-playbook -vvvv sample_playbook.yml -i free-int-master,
+ansible-playbook -vvvv sample_playbook.yml -i $1,
