35fbac461b3effbe6233400fb137d030ec75f244
Justin Pierce <jupierce@redhat.com>
2019-07-11 16:17:56 -0400

    Improving ansible support

--- ansible/roles/openshift_client_python/library/openshift_client_python.py
+++ ansible/roles/openshift_client_python/library/openshift_client_python.py
@@ -17,6 +17,7 @@ import ansible.module_utils.openshift.selector as ___selector
 import ansible.module_utils.openshift.util as ___util
 import ansible.module_utils.openshift.config as ___config
 import ansible.module_utils.openshift.status as ___status
+import ansible.module_utils.openshift.ansible as ___ansible
 
 # Now actually import the package we need
 import ansible.module_utils.openshift as oc
@@ -34,21 +35,20 @@ def main():
             vars=dict(required=False, default={}, type='dict'),
             project=dict(required=False, default=None),
             timeout=dict(required=False, default=None, type='int'),
-            changes=dict(required=False, default=True, type='bool')
+            changes=dict(required=False, default=False, type='bool')
         )
     )
 
     script = module.params["script"]
     time = module.params["timeout"]
-    vars = module.params["vars"]
+    oc.ansible.reset()
+    oc.ansible.vars = module.params["vars"]
 
     if time is not None:
         time = int(time)  # Allow time to come in as a string
 
     if module.params["project"] is not None:
         oc.context.default_project = module.params["project"]
-
-    new_facts = oc.Model()
 
     with oc.timeout(time):
         with oc.tracking() as ct:
@@ -59,7 +59,7 @@ def main():
                 module.debug("openshift_client_python module invocation result:\n" + str(ct.get_result()))
                 module.exit_json(rc=ct.get_result().status(),
                                  changed=module.params['changes'],
-                                 ansible_facts=new_facts._primitive(),
+                                 ansible_facts=oc.ansible.new_facts,
                                  stdout=capture.out.getvalue().decode('UTF-8'),
                                  stderr=capture.err.getvalue().decode('UTF-8'),
                                  result=ct.get_result().as_dict()
@@ -70,8 +70,8 @@ def main():
                 module.fail_json(msg=ose.msg,
                                  rc=ose.result.status(),
                                  exception_attributes=ose.attributes(),
-                                 changed=module.params['changes'],
-                                 ansible_facts=new_facts._primitive(),
+                                 changed=module.params['changes'] or oc.ansible.changed,
+                                 ansible_facts=oc.ansible.new_facts,
                                  stdout=capture.out.getvalue().decode('UTF-8'),
                                  stderr=capture.err.getvalue().decode('UTF-8'),
                                  result=ct.get_result().as_dict()
--- ansible/run_sample_playbook.sh
+++ ansible/run_sample_playbook.sh
@@ -2,11 +2,10 @@
 
 if [[ -z "$1" ]]; then
     echo "Hostname is required for this operation"
-    echo "Example: $0 int-2"
+    echo "Example: $0 my.bastion.hostname"
     exit 1
 fi
 
 echo "The following sample assumes that 'oc' is on the localhost and can communicate with an OpenShift cluster specified"
 echo "Running..."
-# export PYTHONPATH="$(pwd)/../packages"
 ansible-playbook -vvvv sample_playbook.yml -i $1,
