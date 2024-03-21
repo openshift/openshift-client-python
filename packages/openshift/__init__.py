1667843916c710066338d7076e78682a4d7a6e19
Brad Williams <brad.williams@redhat.com>
2024-03-21 11:45:52 -0400

    Preparing 1.0.23

--- packages/openshift/__init__.py
+++ packages/openshift/__init__.py
@@ -12,7 +12,7 @@ from . import config
 from .ansible import ansible
 
 # Single source for module version
-__VERSION__ = '1.0.22'
+__VERSION__ = '1.0.23'
 
 null = None  # Allow scripts to specify null in object definitions
 
